686400c9cb609b89629ec0be0586cb5da6dbfc5d
Michael May <mike@ull.at>
2024-12-30 17:07:44 +0100

    Basic TicTacToe game

---
+++ src/Console/Program.fs
@@ -0,0 +1,51 @@
+﻿open System
+open TicTacToe
+
+printfn """
+TicTacToe
+
+Insert the coordinates of the square you want to play,
+separated by a space. Ctrl-C to exit.
+
+GameBoard:
+(0 0) (0 1) (0 2)
+(1 0) (1 1) (1 2)
+(2 0) (2 1) (2 2)"""
+
+
+let printBoard (board:Board) =
+    board
+    |> Seq.iter (fun row ->
+        row
+        |> Seq.map (function
+            | Some X -> "X"
+            | Some O -> "O"
+            | None -> " "
+        )
+        |> String.concat " | "
+        |> printfn "%s")
+
+let rec gameLoop (board:Board) =
+    Console.ReadLine().Split(' ')
+    |> Array.map Int32.TryParse
+    |> function
+    | [|true, x; true, y|] ->
+        TicTacToe.setSquare x y board
+        |> function
+        | Error msg ->
+            printfn "%s" msg
+            gameLoop board
+        | Ok newBoard ->
+            printBoard newBoard
+            let winner = TicTacToe.calculateWinner newBoard
+            match winner with
+            | Some player ->
+                printfn "Player %A wins!" player
+            | None ->
+                gameLoop newBoard
+    | _ ->
+        printfn "Please, just insert two numbers separated by a space."
+        gameLoop board
+
+TicTacToe.emptyBoard
+|> gameLoop
---
+++ src/TicTacToe/TicTacToe.fs
@@ -0,0 +1,63 @@
+﻿namespace TicTacToe
+
+type Player = | X | O
+type Square = Player option
+type Board = Square array array
+
+module TicTacToe =
+
+    let emptyBoard : Board =
+        Array.init 3 (fun _ ->
+            Array.init 3 (fun _ ->
+                None))
+
+    let private selectSquares
+        (board:Board)
+        ((a1,a2), (b1,b2), (c1,c2))
+        : Square*Square*Square =
+        board[a1][a2],
+        board[b1][b2],
+        board[c1][c2]
+
+    let private calcualateSquaresWinner = function
+        | Some X, Some X, Some X -> Some X
+        | Some O, Some O, Some O -> Some O
+        | _, _, _ -> None
+
+    let calculateWinner (board:Board) : Player option =
+            // lines
+        [   ((0, 0), (0, 1), (0, 2)); ((1, 0), (1, 1), (1, 2)); ((2, 0), (2, 1), (2, 2))
+            // rows
+            ((0, 0), (1, 0), (2, 0)); ((0, 1), (1, 1), (2, 1)); ((0, 2), (1, 2), (2, 2))
+            // diagonals
+            ((0, 0), (1, 1), (2, 2)); ((0, 2), (1, 1), (2, 0))]
+        |> Seq.map (selectSquares board)
+        |> Seq.map calcualateSquaresWinner
+        |> Seq.choose id
+        |> Seq.tryHead
+
+    let nextPlayer (board:Board) : Player =
+        seq { for x in 0..2 do for y in 0..2 do board[x][y] }
+        |> Seq.fold (fun (xCount,oCount) square ->
+                match square with
+                | Some X -> (xCount + 1, oCount)
+                | Some O -> (xCount, oCount + 1)
+                | None -> (xCount, oCount)
+            ) (0,0)
+        |> fun (xCount,oCount) ->
+            if xCount > oCount then O
+            else X
+
+    let setSquare (x:int) (y:int) (board:Board) : Result<Board, string> =
+        if x < 0 || x > 2 || y < 0 || y > 2 then
+            Error $"Square {x}/{y} invalid"
+        elif board[x][y] <> None then
+            Error $"Square {x}/{y} already taken"
+        else
+            let nextPlayer = nextPlayer board
+            board
+            |> Array.updateAt x (
+                board.[x]
+                |> Array.updateAt y
+                    (Some nextPlayer))
+            |> Ok
